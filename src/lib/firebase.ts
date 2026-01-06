d038301ebe3bb19f5c1ea156d8ae52954cdeef15
delinda <delindamegaputri123@gmail.com>
2026-01-06 18:22:33 +0700

    update sesuatu

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCVBeazojSe8p-y5lF3m1erFVECGihGPC8",
   authDomain: "project-iot-14988.firebaseapp.com",
   databaseURL: "https://project-iot-14988-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "project-iot-14988",
-  storageBucket: "project-iot-14988.firebasestorage.app",
+  storageBucket: "project-iot-14988.appspot.com",
   messagingSenderId: "47065380086",
   appId: "1:47065380086:web:4b275b9d23cd17a39ae8d7",
-  measurementId: "G-2DCC207NKW"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const database = getDatabase(app);
